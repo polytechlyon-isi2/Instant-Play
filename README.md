5bc33c533de7d5039ca35e371d83467acedf0fdd
gregpiatperso <gregoire.piat@outlook.fr>
2016-02-11 17:30:18 +0100

    Fork from first project repo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Instant-Play
+# ISI2-ProjetLaravel
+Projet de création de site web e-commerce avec le framwork Laravel (v5.2).
+
+# Nicolas Diab et Grégoire Piat
---
+++ app/Http/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Routes pour toute la partie administration
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+    //TODO Ajouter ici les routes pour l'administration
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
