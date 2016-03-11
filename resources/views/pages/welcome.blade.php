ca5d40cddbb24074414a622d2488d5cc84ad620f
NicolasDiab <nicolas-diab@hotmail.fr>
2016-03-11 10:56:04 +0100

    modif de la page pour ajouter un yield('content')

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,17 +1,192 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
+    <h3>Browse All Categories</h3>
+    <div class="section group">
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img1.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
                 </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img2.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img3.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img4.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
             </div>
         </div>
     </div>
-</div>
+    <div class="section group">
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img1.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img2.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img3.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img4.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+    <div class="section group">
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img1.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img2.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img3.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+
+        </div>
+        <div class="grid_1_of_4 images_1_of_4">
+            <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+            <a href="preview.html"><img src="images/product-img4.jpg" alt="" /></a>
+            <div class="price-details">
+                <div class="price-number">
+                    <p><span class="rupees">$839.93 </span></p>
+                </div>
+                <div class="add-cart">
+                    <h4><a href="preview.html">More Info</a></h4>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+    <div class="product-articles">
+        <h3>Browse All Categories</h3>
+        <ul>
+            <li>
+                <div class="article">
+                    <p><span>Aenean vitae massa dolor</span></p>
+                    <p>Phasellus in quam dui. Nunc ornare, tellus rutrum porttitor imperdiet, dui leo molestie nisl, sit amet dignissim nibh magna pharetra quam. Nunc ultrices pellentesque massa, ac adipiscing dui rutrum id. In cursus augue non erat faucibus eu condimentum dolor molestie.</p>
+                    <p><a href="#">+ Read Full article</a></p>
+                </div>
+            </li>
+            <li>
+                <div class="article">
+                    <p><span>Phasellus sollicitudin consectetur</span></p>
+                    <p>Cras aliquam, odio ac consectetur tincidunt, eros nunc fermentum augue, quis rutrum ante lectus ac lectus. Fusce sed tellus orci, et feugiat urna. Integer et dictum leo. Nulla consectetur tempus orci sed consequat. Mauris cursus est a sapien venenatis faucibus. Etiam sagittis convallis volutpat.</p>
+                    <p><a href="#">+ Read Full article</a></p>
+                </div>
+            </li>
+        </ul>
+    </div>
 @endsection
