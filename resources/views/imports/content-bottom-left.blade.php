cc89d3dc5a9929a68c4f47c2c35c9cb22321f023
NicolasDiab <nicolas-diab@hotmail.fr>
2016-03-11 10:54:41 +0100

    sidebar for categories

---
+++ resources/views/imports/content-bottom-left.blade.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas
+ * Date: 11/03/2016
+ * Time: 10:42
+ */
+?>
+
+<div class="categories">
+    <ul>
+        <h3>Browse All Categories</h3>
+        <li><a href="#">Appliances</a></li>
+        <li><a href="#">Sports Equipments</a></li>
+        <li><a href="#">Computers & Electronics</a></li>
+        <li><a href="#">Office supplies</a></li>
+        <li><a href="#">Health & Beauty</a></li>
+        <li><a href="#">Home & Garden</a></li>
+        <li><a href="#">Apparel</a></li>
+        <li><a href="#">Toys & Games</a></li>
+        <li><a href="#">Automotive</a></li>
+    </ul>
+</div>
+<div class="buters-guide">
+    <h3>Buyers Guide</h3>
+    <p><span>We want you to be happy with your purchase.</span></p>
+    <p>So we're committed to giving you all the tools to make the right decision with minimum fuss. </p>
+</div>
+<div class="add-banner">
+    <img src="images/camera.png" alt="" />
+    <div class="banner-desc">
+        <h4>Electronics</h4>
+        <a href="#">More Info</a>
+    </div>
+    <div class="clear"></div>
+</div>
+<div class="add-banner add-banner2">
+    <img src="images/computer.png" alt="" />
+    <div class="banner-desc">
+        <h4>Computers</h4>
+        <a href="#">More Info</a>
+    </div>
+    <div class="clear"></div>
+</div>
