83d0d8df2571bbf06cd093649c1e625933cd4e13
Nicolas Diab <nicolas-diab@hotmail.fr>
2016-03-23 12:02:50 +0100

    exemple d'affichage de jeu sur la page d'accueil

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -17,18 +17,30 @@
                     <div class="clear"></div>
                 </div>
             </div>
+
+
+
+
+
+            @foreach($games as $game)
             <div class="grid_1_of_4 images_1_of_4">
-                <h4><a href="preview.html">Whirlpool LTE5243D 3.4 CuFt.... </a></h4>
+                <h4><a href="preview.html">{{ $game->name }}</a></h4>
                 <a href="preview.html"><img src="images/product-img2.jpg" alt="" /></a>
                 <div class="price-details">
                     <div class="price-number">
-                        <p><span class="rupees">$839.93 </span></p>
+                        <p><span class="rupees">{{ $game->price }}€ </span></p>
                     </div>
                     <div class="add-cart">
                         <h4><a href="preview.html">More Info</a></h4>
                     </div>
                     <div class="clear"></div>
                 </div>
+            @endforeach
+
+
+
+
+
 
             </div>
             <div class="grid_1_of_4 images_1_of_4">
